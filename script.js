6595cd87398fc33fc58e3081ad6452f53e56c9e7
Helloimalex1223 <plattaalex@gmail.com>
2024-05-09 09:24:22 -0900

    Add styling, add html elements, add JS function

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function validatePassword() 
-{
-     let a = document.querySelector('#password').value;
-     let b = document.querySelector('#password-confirm').value;
-     
-     if(a == b)
-     {
-        alert("Good job! The passwords are the same.");
-     }
-     else
-     {
-        alert("The passwords are not the same :(.");
-     }
-}
+const myForm = document.querySelector("#myForm");
+myForm.noValidate = true;
 
+myForm.addEventListener('submit', validateForm);
 
-document.querySelector("#create").addEventListener("click", validatePassword());
+function validateForm(e) {
+
+    const form = e.target;
+  
+    if (form.checkValidity()) {
+  
+      // form is valid - make further checks
+  
+    }
+    else {
+  
+      // form is invalid - cancel submit
+      e.preventDefault();
+  
+    }
+  
+  };
